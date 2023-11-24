c853c14866ecf4d2d654d9bca3f6b95f0350f215
HerrBergenholtz <zbdv8800@gmail.com>
2023-11-24 11:14:21 +0100

    Add project files.

---
+++ C Uppgift 5.7/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    public static void Main()
+    {
+        string[] länder = { "Kina", "Indien", "USA", "Indonesien", "Pakistan" };
+        
+        for (int i = 0; i < länder.Length; i++)
+        {
+            Console.WriteLine($"{i + 1}. {länder[i]}");
+        }
+
+        try
+        {
+            Console.WriteLine("Vilka länder vill du ska visas igen? (skriv in landens siffror separerat av mellanslag");
+            string[] input = Console.ReadLine().Split(" ");
+            int[] nummer = new int[input.Length];
+
+            for (int i = 0;i < input.Length;i++)
+            {
+                nummer[i] = int.Parse(input[i]) - 1;
+            }
+        
+            for (int i = 0; i < nummer.Length ; i++) 
+            {
+                Console.WriteLine($"{nummer[i] + 1}. {länder[nummer[i]]}");
+            }
+        }
+
+        catch
+        {
+            Console.WriteLine("Fel input, vänligen pröva igen");
+            Console.ReadKey();
+            Main();
+        }
+    }
+}
